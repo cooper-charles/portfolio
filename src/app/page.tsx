11448a34da03de807858b15bbda841a6124d4d08
cooper-charles <ccharl10@vols.utk.edu>
2026-08-19 20:29:23 -0400

    added navbar and started main page content

---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[#101014] text-white">
+      <main className="max-w-3xl mx-auto px-4 py-12 flex flex-col gap-12">
+        <section>
+          <div className="grid grid-cols-[1fr_1fr]">
+            <h1 className="text-2xl font-bold font-heading text-4xl">Cooper Charles</h1>
+            <div className="flex items-center justify-center gap-1 sm:gap-2 text-xs sm:text-sm overflow-x-auto no-scrollbar">
+              <Link href="/about" className="px-2 sm:px-3 py-1.5 grid grid-cols-[1fr_1fr]">
+                <img src="/file.svg" width="25" height="15"/>
+                <p>Resume</p>
+              </Link>
+              <Link href="/experience" className="px-2 sm:px-3 py-1.5">
+                Experience
+              </Link>
+              <Link href="/projects" className="px-2 sm:px-3 py-1.5">
+                Projects
+              </Link>
+              <Link href="/skills" className="px-2 sm:px-3 py-1.5">
+                Skills
+              </Link>
+            </div>
+          </div>
+          <p className="text-neutral-400 mt-1">
+            Nashville, TN
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-xl font-bold mb-4">What I work with</h2>
+        </section>
+
+      </main>
+
+    </div>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+export function Navbar() {
+    return (
+        <nav className="sticky top-0 z-50 bg-[#101014] w-full">
+            <div className="max-w-7xl mx-auto px-4 h-16 grid grid-cols-[1fr_auto_1fr] items-center font-heading font-semibold text-white">
+                <Link href="/" className="font-bold">
+                    Cooper Charles
+                </Link>
+                <div className="flex items-center justify-center gap-1 sm:gap-2 text-xs sm:text-sm overflow-x-auto no-scrollbar">
+                    <Link href="/about" className="px-2 sm:px-3 py-1.5">
+                      About
+                    </Link>
+                    <Link href="/experience" className="px-2 sm:px-3 py-1.5">
+                      Experience
+                    </Link>
+                    <Link href="/projects" className="px-2 sm:px-3 py-1.5">
+                      Projects
+                    </Link>
+                    <Link href="/skills" className="px-2 sm:px-3 py-1.5">
+                      Skills
+                    </Link>
+                </div>
+                <Link href="/contact" className="justify-self-end">
+                    Contact
+                </Link>
+            </div>
+        </nav>
+    )
+}
