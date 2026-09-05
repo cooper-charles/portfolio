b7ab2775a08b69767f99aadd0d6cce7eb4f099be
cooper-charles <ccharl10@vols.utk.edu>
2026-09-05 12:41:09 -0400

    added vercel analytics support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, Inter } from 'next/font/google'
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 const montserrat = Montserrat({
   subsets: ['latin'],
